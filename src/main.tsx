cda822183b4f8c955e0bcc4848181b3234879cd6
decentinnfamily <decentinnfamily@gmail.com>
2026-05-15 20:15:37 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
+import { inject } from '@vercel/analytics';
 import App from './App.tsx';
 import './index.css';
+
+inject();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
